3656252b3908ff7ae75f8b7795bad16278b65062
ryuju1029 <ryuju19971029@gmail.com>
2023-08-29 11:12:03 +0900

    feat: usecaseの実装

---
+++ repository/user_repository.go
@@ -0,0 +1,28 @@
+package repository
+
+import (
+	"go-rest-api/model"
+
+	"gorm.io/gorm"
+)
+
+type IUserRepository interface {
+	GetUserByEmail(user *model.User, email string) error
+	CreateUser(user *model.User) error
+}
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) IUserRepository {
+	return &userRepository{db}
+}
+
+func (ur *userRepository) GetUserByEmail(user *model.User, email string) error {
+	return nil
+}
+
+func (ur *userRepository) CreateUser(user *model.User) error {
+	return nil
+}
