59b37d342e312d6439cf53bb2838dd5e888fa9b2
ryuju1029 <ryuju19971029@gmail.com>
2023-09-01 09:14:38 +0900

    feat: taskControllerの実装

--- controller/task_controller.go
+++ controller/task_controller.go
@@ -1,6 +1,15 @@
 package controller
 
-import "github.com/labstack/echo/v4"
+import (
+	"go-rest-api/model"
+	"go-rest-api/usecase"
+	"net/http"
+	"strconv"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/labstack/echo/v4"
+)
 
 type ITaskController interface {
 	GetAllTasks(c echo.Context) error
@@ -8,4 +17,74 @@ type ITaskController interface {
 	CreateTask(c echo.Context) error
 	UpdateTask(c echo.Context) error
 	DeleteTask(c echo.Context) error
+}
+
+type taskController struct {
+	tu usecase.ITaskUsecase
+}
+
+func NewTaskController(tu usecase.ITaskUsecase) ITaskController {
+	return &taskController
+}
+
+func (tc *taskController) GetAllTasks(c echo.Context) error {
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	userId := claims["user_id"]
+
+	tasksRes, err := tc.tu.GetAllTasks(uint(userId.(float64)))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	return c.JSON(http.StatusOK, tasksRes)
+}
+
+func (tc *taskController) GetTaskById(c echo.Context) error {
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	userId := claims["user_id"]
+	id := c.Param("taskId")
+	taskId, _ := strconv.Atoi(id)
+	taskRes, err := tc.tu.GetTaskById(uint(userId.(float64)), uint(uint(taskId)))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, taskRes)
+}
+
+func (tc *taskController) CreateTask(c echo.Context) error {
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	userId := claims["user_id"]
+
+	task := model.Task{}
+	if err := c.Bind(&task); err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	task.UserId = uint(userId.(float64))
+	taskRes, err := tc.tu.CreateTask(task)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, taskRes)
+}
+
+func (tc *taskController) UpdateTask(c echo.Context) error {
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	userId := claims["user_id"]
+	id := c.Param("taskId")
+	taskId, _ := strconv.Atoi(id)
+
+	task := model.Task{}
+	if err := c.Bind(&task); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+	taskRes, err := tc.tu.UpdateTask(task, uint(userId(float64)), uint(taskId))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	return c.JSON(http.StatusOK, taskRes)
 }
